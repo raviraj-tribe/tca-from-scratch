65430588d62f18b9142393d8f8c40e6ca8091091
raviraj-tribe <77301579+raviraj-tribe@users.noreply.github.com>
2024-07-03 13:25:43 +0200

    init

---
+++ SyncUps/SyncUps/SyncUpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncUpsApp.swift
+//  SyncUps
+//
+//  Created by Raviraj Minawala on 03.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SyncUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
